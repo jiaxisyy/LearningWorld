3de2b3d88c0e386a9637e37f999cbf541a5df1a6
hekd <xyzsyy0102@163.com>
2017-12-09 16:02:56 +0800

    采用ijkplayer播放视频,在记忆猫上相对原生较为流畅,部分界面掉帧,在联想手机上无任何问题

--- app/src/main/java/com/example/hekd/learningworld/update2/MyijkplayerActivity.kt
+++ app/src/main/java/com/example/hekd/learningworld/update2/MyijkplayerActivity.kt
@@ -1,93 +1,75 @@
 package com.example.hekd.learningworld.update2
 
 import android.os.Bundle
+import android.view.SurfaceHolder
 import com.example.hekd.learningworld.R
-import com.example.hekd.learningworld.update2.interf.VideoPlayerListener
-import com.example.hekd.learningworld.update2.v.IView
-import com.example.hekd.learningworld.update2.view.IjkVideoView
 import com.zhy.autolayout.AutoLayoutActivity
-import kotlinx.android.synthetic.main.avtivity_ijk.*
+import kotlinx.android.synthetic.main.activity_ijk.*
+
 import tv.danmaku.ijk.media.player.IMediaPlayer
 import tv.danmaku.ijk.media.player.IjkMediaPlayer
+import java.io.IOException
 
 /**
  * Created by hekd on 2017/11/28.
  */
-class MyijkplayerActivity : AutoLayoutActivity(), IView {
-    override fun videoStart() {
+class MyijkplayerActivity : AutoLayoutActivity(), SurfaceHolder.Callback, IMediaPlayer.OnPreparedListener, IMediaPlayer.OnCompletionListener, IMediaPlayer.OnBufferingUpdateListener {
+
+    var ijkPlayer: IjkMediaPlayer? = null
+
+
+    override fun onPrepared(p0: IMediaPlayer?) {
+        ijkPlayer!!.start()
     }
 
-    override fun videoStop() {
+    override fun onCompletion(p0: IMediaPlayer?) {
+        ijkPlayer!!.seekTo(0)
+        ijkPlayer!!.start()
     }
 
-    override fun videoPause() {
+    override fun onBufferingUpdate(p0: IMediaPlayer?, p1: Int) {
     }
 
-    override fun hideControl() {
+    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
     }
 
-    override fun showControl() {
+    override fun surfaceDestroyed(p0: SurfaceHolder?) {
     }
 
-    override fun fullScreen() {
+    override fun surfaceCreated(p0: SurfaceHolder?) {
+
+        ijkPlayer!!.setDisplay(p0)
+        ijkPlayer!!.prepareAsync()
     }
 
-    override fun nextVideo() {
-    }
 
-    override fun previousVideo() {
-    }
-
-    var ijkPlayer: IjkVideoView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        try {
-//            IjkMediaPlayer.loadLibrariesOnce(null)
-//            IjkMediaPlayer.native_profileBegin("libijkplayer.so")
-//        } catch (e: Exception) {
-//            finish()
-//        }
+        setContentView(R.layout.activity_ijk)
         val path = intent.getStringExtra("ijk_video_path")
 
-        ijkPlayer = IjkVideoView(this)
-        loadVideo(path)
-        ijkPlayer!!.setListener(object :VideoPlayerListener(){
-            override fun onBufferingUpdate(p0: IMediaPlayer?, p1: Int) {
-            }
+        sv_ijk.holder.addCallback(this)
+        ijkPlayer = IjkMediaPlayer()
+        //开启硬解码
+        ijkPlayer!!.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec", 1)
 
-            override fun onCompletion(p0: IMediaPlayer?) {
-                p0!!.seekTo(0)
-                p0.start()
-            }
+        try {
+            ijkPlayer!!.dataSource = path
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
 
-            override fun onPrepared(p0: IMediaPlayer?) {
-                p0!!.start()
-            }
-
-            override fun onInfo(p0: IMediaPlayer?, p1: Int, p2: Int): Boolean = false
-
-            override fun onVideoSizeChanged(p0: IMediaPlayer?, p1: Int, p2: Int, p3: Int, p4: Int) {
-
-            }
-
-            override fun onError(p0: IMediaPlayer?, p1: Int, p2: Int): Boolean = false
-
-            override fun onSeekComplete(p0: IMediaPlayer?) {
-
-            }
-        })
-
-//        ijkPlayer!!.start()
+        //mediaPlayer准备工作
+        ijkPlayer!!.setOnPreparedListener(this)
+        //MediaPlayer完成
+        ijkPlayer!!.setOnCompletionListener(this)
+        //当前加载进度的监听
+        ijkPlayer!!.setOnBufferingUpdateListener(this)
     }
 
-    fun loadVideo(path: String) {
-        ijkPlayer!!.setVideoPath(path)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        IjkMediaPlayer.native_profileEnd()
+    override fun onDestroy() {
+        super.onDestroy()
+        ijkPlayer!!.stop()
     }
 
 
